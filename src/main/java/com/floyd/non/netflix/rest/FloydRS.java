45ce257ec0c10069dafbfb55579b6bb8f3fc95fb
Dibyarup Basu <dibyarup.basu@infosys.com>
2016-11-27 18:44:02 +0530

    Draft Commit 0.1

---
+++ src/main/java/com/floyd/non/netflix/rest/FloydRS.java
@@ -0,0 +1,35 @@
+package com.floyd.non.netflix.rest;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FloydRS {
+
+	@RequestMapping("/")
+	public String index() {
+		return "\"Hey You!!\" - David Gilmour";
+	}
+
+	@RequestMapping(value = "/dark-side-of-the-moon", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<InputStreamResource> showLogo() throws IOException {
+	    URL url = new URL("http://i.ytimg.com/vi/NJQnzmH6jgc/maxresdefault.jpg");
+	    URLConnection conn = url.openConnection();
+	    
+	    return ResponseEntity.ok()
+	            .contentLength(2048*2048)
+	            .contentType(MediaType.IMAGE_JPEG)
+	            .body(new InputStreamResource(conn.getInputStream()));
+	}
+
+}
